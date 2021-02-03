ccbdad3f503db3461c5313711bb3d0590adca3c2
redyetidev <38299977+RedYetiDev@users.noreply.github.com>
2021-02-02 20:06:09 -0500

    Added games

---
+++ game.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+  var span = document.getElementsByClassName("close")[0];
+  var modal = document.getElementById("myModal");
+  g1 = document.getElementById("game1")
+  g2 = document.getElementById("game2")
+  games = document.getElementsByClassName("games")[0]
+  window.onresize = function() {
+    if (window.innerWidth < 875 & games.style.display != "none") {
+      console.log("RESIZE")
+      games.style.display = "none"
+    }
+    else if (window.innerWidth > 874 & games.style.display == "none") {
+      console.log("RESIZE (2)")
+      games.style.display = "initial"
+    }
+  }
+
+  modal.style.display = "block";
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  g1.onwebkitfullscreenchange = function() {g1.src = g1.src}
+  g2.onwebkitfullscreenchange = function() {g2.src = g2.src}
+}
+function full(elem) {
+  if (elem.requestFullscreen) {
+  elem.requestFullscreen();
+} else if (elem.webkitRequestFullscreen) { /* Safari */
+  elem.webkitRequestFullscreen();
+} else if (elem.msRequestFullscreen) { /* IE11 */
+  elem.msRequestFullscreen();
+}
+}
