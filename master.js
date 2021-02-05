54e64f3e7bb98768ff1359b210fafa849dca394d
redyetidev <38299977+RedYetiDev@users.noreply.github.com>
2021-02-05 13:00:53 -0500

    Change

--- master.js
+++ master.js
@@ -1,3 +1,15 @@
+function changecursor(e) {
+  filenum = Math.floor(Math.random() * Math.floor(675))
+  document.body.style.cursor = 'url("https://raw.githubusercontent.com/RedYetiDev/First-Transcontinental-Railroad/main/imgvid/cursors/"' + filenum + '.ani), default';
+}
+
+window.onload = function() {
+  document.body.addEventListener("keyup", function(event) {
+      if (event.keyCode == 9) {
+        changecursor()
+      }
+  })
+}
 $(document).ready(function(){
     $("body").hide(0).delay(200).fadeIn(1500)
 });
